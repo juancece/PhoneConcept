97ecbda9e9dbf8d45a25712862cb19aed4ce733a
Juan Carmona <juan.carmona@bairesdev.com>
2020-05-20 21:41:45 -0500

    Update 1

---
+++ PhoneConcept/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhoneConcept.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PhoneConcept/Models/PhoneNumberCheckViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PhoneConcept.Models
+{
+    public class PhoneNumberCheckViewModel
+    {
+        private string _countryCodeSelected;
+
+        [Required]
+        [Display(Name = "Issuing Country")]
+        public string CountryCodeSelected
+        {
+            get => _countryCodeSelected;
+            set => _countryCodeSelected = value?.ToUpperInvariant();
+        }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [MaxLength(18)]
+        public string PhoneNumberRaw { get; set; }
+
+        [Display(Name = "Valid Number")]
+        public bool Valid { get; set; }
+
+        [Display(Name = "Validated Type")]
+        public string PhoneNumberType { get; set; }
+
+        [Display(Name = "Region Code")]
+        public string RegionCode { get; set; }
+
+        [Display(Name = "International Dialing Format")]
+        public string PhoneNumberFormatted { get; set; }
+
+        [Display(Name = "Mobile Dialing Format")]
+        public string PhoneNumberMobileDialing { get; set; }
+
+        public IEnumerable<SelectListItem> Countries { get; set; }
+    }
+}
